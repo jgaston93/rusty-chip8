9b7db8c1b3f5847975d34e88d9475db43c08e357
jgast <jgaston93@gmail.com>
2026-03-29 13:40:30 +0000

    adding ggez crate

--- src/platform.rs
+++ src/platform.rs
@@ -1,2 +1,25 @@
+use ggez::{Context, ContextBuilder, GameResult};
+use ggez::graphics::{self, Color};
+use ggez::event::{self, EventHandler};
+
 pub struct Platform {
+    ctx: Context,
 }
+
+impl Platform{
+    pub fn process_input(keys: &mut [u8; 16]) {
+    }
+}
+
+impl EventHandler for Platform {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
+        canvas.finish(ctx)
+    }
+    
+}
+
