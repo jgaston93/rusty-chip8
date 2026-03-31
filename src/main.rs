17c658876a3a15e9fe1062235b5407a4e8ccdbdf
jgast <jgaston93@gmail.com>
2026-03-31 03:32:35 +0000

    testing key_down_event

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,18 @@ mod chip8;
 mod platform;
 use crate::chip8::Chip8;
 use crate::platform::Platform;
+use ggez::{conf, GameResult, event};
 
-fn main() {
+fn main() -> GameResult {
     let mut cpu: Chip8;
-    let mut platform: Platform;
+    let mut platform: Platform = Platform {};
+
+        let cb = ggez::ContextBuilder::new("input_test", "ggez").window_mode(
+        conf::WindowMode::default()
+            .fullscreen_type(conf::FullscreenType::Windowed)
+            .resizable(true),
+    );
+    let (ctx, event_loop) = cb.build()?;
+
+    event::run(ctx, event_loop, platform)
 }
--- src/platform.rs
+++ src/platform.rs
@@ -1,14 +1,15 @@
 use ggez::{Context, ContextBuilder, GameResult};
 use ggez::graphics::{self, Color};
 use ggez::event::{self, EventHandler};
+use ggez::input::keyboard::KeyInput;
 
 pub struct Platform {
-    ctx: Context,
 }
 
 impl Platform{
-    pub fn process_input(keys: &mut [u8; 16]) {
-    }
+    pub fn process_input(self, k: &mut [u8; 16]) {
+        // k.copy_from_slice(&self.keys); 
+   }
 }
 
 impl EventHandler for Platform {
@@ -19,6 +20,18 @@ impl EventHandler for Platform {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
         canvas.finish(ctx)
+    }
+
+    fn key_down_event(&mut self, ctx: &mut Context, input: KeyInput, repeat: bool) -> GameResult {
+        println!(
+            "Key pressed: physical key {:?}, logical key {:?}, modifier {:?}, repeat: {}",
+            input.event.physical_key, input.event.logical_key, input.mods, repeat
+        );
+        Ok(())
+    }
+
+    fn key_up_event(&mut self, ctx: &mut Context, input: KeyInput) -> GameResult {
+        Ok(())
     }
     
 }
